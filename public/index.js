b065624cbf5bac66237008ab57851d9de9222a6f
Chris <104800006+renners7777@users.noreply.github.com>
2023-12-19 18:45:28 +0000

    Changed file structure

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000; // You can use any port you prefer
+app.use(express.static('public'))
 
 // List of Christmas-related words
 const christmasWords = [
