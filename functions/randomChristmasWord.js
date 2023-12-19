4b4497997907cbdb4dfabf9bf939e352f387bb12
Chris <104800006+renners7777@users.noreply.github.com>
2023-12-19 16:23:04 +0000

    Added netlify function and js file to this function

---
+++ functions/randomChristmasWord.js
@@ -0,0 +1,20 @@
+// functions/randomWord.js
+
+const christmasWords = [
+    "Santa", "Reindeer", "Gifts", "Christmas Tree",
+    "Snowflake", "Mistletoe", "Jingle Bells", "Candy Cane",
+    "Frosty", "Sleigh", "Stockings", "Gingerbread", "Joy", "Peace"
+];
+
+exports.handler = async () => {
+    const randomIndex = Math.floor(Math.random() * christmasWords.length);
+    const randomChristmasWord = christmasWords[randomIndex];
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ random_word: randomChristmasWord }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+};
